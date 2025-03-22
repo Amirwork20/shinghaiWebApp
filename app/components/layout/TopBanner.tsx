d089961393d016cb0f2ef82ea062e2753a007571
Saqlain Zafar <saqlainzafar342176@gmail.com>
2025-03-23 04:58:53 +0500

    ok

--- app/components/layout/TopBanner.tsx
+++ app/components/layout/TopBanner.tsx
@@ -1,6 +1,6 @@
 export default function TopBanner() {
   return (
-    <div className="text-white text-center py-1 text-sm" style={{ backgroundColor: "#002a2a" }}>
+    <div className="text-white text-center py-1 text-sm" style={{ backgroundColor: "black" }}>
       Free delivery for orders above Rs.4999 within Pakistan
     </div>
   )
